06487a5f940e0308855099d46f42d39a6132331e
Zablah <miguel12979@gmail.com>
2018-04-27 16:38:54 -0500

    Change file content types to json in appsettings

--- CMCore/Helpers/FileSettings.cs
+++ CMCore/Helpers/FileSettings.cs
@@ -1,12 +1,16 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using Newtonsoft.Json.Linq;
 
 namespace CMCore.Models
 {
     public class FileSettings
     {
         public string[] AcceptedFileTypes { get; set; }
+        public IDictionary<string, string> ContentTypes { get; set; }
 
         public bool IsSupported(string fileName)
         {
@@ -15,30 +19,9 @@ namespace CMCore.Models
 
         public string GetContentType(string path)
         {
-            var types = GetTypesDir();
             var ext = Path.GetExtension(path).ToLowerInvariant();
 
-            return !types.ContainsKey(ext) ? null : types[ext];
-        }
-
-        private Dictionary<string, string> GetTypesDir()
-        {
-            return new Dictionary<string, string>
-            {
-                {".txt", "text/plain"},
-                {".pdf", "application/pdf"},
-                {".doc", "application/vnd.ms-word"},
-                {".docx", "application/vnd.ms-word"},
-                {".xls", "application/vnd.ms-excel"},
-                {".xlsx", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"},
-                {".png", "image/png"},
-                {".jpg", "image/jpeg"},
-                {".jpeg", "image/jpeg"},
-                {".gif", "image/gif"},
-                {".csv", "text/csv"},
-                {".mp3", "application/mp3"},
-                {".mp4", "video/mp4"}
-            };
+            return !ContentTypes.ContainsKey(ext) ? null : ContentTypes[ext];
         }
 
     }
