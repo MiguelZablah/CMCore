731726cdd04b75bf72753e1e30ea2cac6eec15b0
Zablah <miguel12979@gmail.com>
2018-07-25 13:03:20 -0500

    Update packages: AWS and Auttomapper to lattes

--- CMCore/Data/AutoMapperProfile.cs
+++ CMCore/Data/AutoMapperProfile.cs
@@ -10,53 +10,58 @@ namespace CMCore.Data
     {
         public AutoMapperProfile()
         {
-            // Domain to Dto
-            CreateMap<File, FileDto>()
-                .ForMember(dto => dto.Tags, opt => opt.MapFrom(ft => ft.FileTags.Select(t => t.Tag)))
-                .ForMember(dto => dto.Clubs, opt => opt.MapFrom(fc => fc.FileClubs.Select(c => c.Club)))
-                .ForMember(dto => dto.Companies, opt => opt.MapFrom(fco => fco.FileCompanies.Select(c => c.Companie)))
-                .ForMember(dto => dto.Extension, opt => opt.MapFrom(fco => fco.Extension.Name));
+            Mapper.Initialize(cfg =>
+            {
+                // Domain to Dto
+                cfg.CreateMap<File, FileDto>()
+                    .ForMember(dto => dto.Tags, opt => opt.MapFrom(ft => ft.FileTags.Select(t => t.Tag)))
+                    .ForMember(dto => dto.Clubs, opt => opt.MapFrom(fc => fc.FileClubs.Select(c => c.Club)))
+                    .ForMember(dto => dto.Companies, opt => opt.MapFrom(fco => fco.FileCompanies.Select(c => c.Companie)))
+                    .ForMember(dto => dto.Extension, opt => opt.MapFrom(fco => fco.Extension.Name));
 
-            CreateMap<Tag, TagDto>();
+                cfg.CreateMap<Tag, TagDto>();
 
-            CreateMap<Club, ClubDto>()
-                .ForMember(dto => dto.Regions, opt => opt.MapFrom(cr => cr.ClubRegions.Select(r => r.Region)))
-                .ForMember(dto => dto.Types, opt => opt.MapFrom(fty => fty.ClubTypes.Select(ty => ty.Type)));
+                cfg.CreateMap<Club, ClubDto>()
+                    .ForMember(dto => dto.Regions, opt => opt.MapFrom(cr => cr.ClubRegions.Select(r => r.Region)))
+                    .ForMember(dto => dto.Types, opt => opt.MapFrom(fty => fty.ClubTypes.Select(ty => ty.Type)));
 
-            CreateMap<Region, RegionDto>();
+                cfg.CreateMap<Region, RegionDto>();
 
-            CreateMap<Countrie, CountrieDto>();
+                cfg.CreateMap<Countrie, CountrieDto>();
 
-            CreateMap<Type, TypeDto>();
+                cfg.CreateMap<Type, TypeDto>();
 
-            CreateMap<Extension, ExtensionDto>();
+                cfg.CreateMap<Extension, ExtensionDto>();
 
-            CreateMap<Companie, CompanieDto>();
+                cfg.CreateMap<Companie, CompanieDto>();
 
-            // Dto to Domain
-            CreateMap<FileDto, File>()
-                .ForMember(f => f.Id, opt => opt.Ignore());
+                // Dto to Domain
+                cfg.CreateMap<FileDto, File>()
+                    .ForMember(f => f.Id, opt => opt.Ignore());
 
-            CreateMap<TagDto, Tag>()
-                .ForMember(t => t.Id, opt => opt.Ignore());
+                cfg.CreateMap<TagDto, Tag>()
+                    .ForMember(t => t.Id, opt => opt.Ignore());
 
-            CreateMap<ClubDto, Club>()
-                .ForMember(c => c.Id, opt => opt.Ignore());
+                cfg.CreateMap<ClubDto, Club>()
+                    .ForMember(c => c.Id, opt => opt.Ignore());
 
-            CreateMap<RegionDto, Region>()
-                .ForMember(r => r.Id, opt => opt.Ignore());
+                cfg.CreateMap<RegionDto, Region>()
+                    .ForMember(r => r.Id, opt => opt.Ignore());
 
-            CreateMap<CountrieDto, Countrie>()
-                .ForMember(c => c.Id, opt => opt.Ignore());
+                cfg.CreateMap<CountrieDto, Countrie>()
+                    .ForMember(c => c.Id, opt => opt.Ignore());
 
-            CreateMap<TypeDto, Type>()
-                .ForMember(t => t.Id, opt => opt.Ignore());
+                cfg.CreateMap<TypeDto, Type>()
+                    .ForMember(t => t.Id, opt => opt.Ignore());
 
-            CreateMap<ExtensionDto, Extension>()
-                .ForMember(e => e.Id, opt => opt.Ignore());
+                cfg.CreateMap<ExtensionDto, Extension>()
+                    .ForMember(e => e.Id, opt => opt.Ignore());
 
-            CreateMap<CompanieDto, Companie>()
-                .ForMember(c => c.Id, opt => opt.Ignore());
+                cfg.CreateMap<CompanieDto, Companie>()
+                    .ForMember(c => c.Id, opt => opt.Ignore());
+            });
+
+            
         }
 
     }
